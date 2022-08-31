52942c97a3eb01cf6c6b47827f4feaeb58d31874
albuquerquealdry <albuquerquealdry@gmail.com>
2022-08-30 22:18:20 -0300

    [DEVOPS]-[ADD LOCALSTACK]

--- main.tf
+++ main.tf
@@ -2,15 +2,67 @@
 
 // PRINCIPAL RESOURCE
 provider "aws" {
-    version = "~> 2.0"
-    region = "us-east-1"
+  region                      = "us-east-1"
+  access_key                  = "foo"
+  secret_key                  = "bar"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  endpoints {
+    apigateway     = "http://localhost:4566"
+    cloudformation = "http://localhost:4566"
+    cloudwatch     = "http://localhost:4566"
+    dynamodb       = "http://localhost:4566"
+    es             = "http://localhost:4566"
+    ec2            = "http://localhost:4566"
+    firehose       = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    kinesis        = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    route53        = "http://localhost:4566"
+    redshift       = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    secretsmanager = "http://localhost:4566"
+    ses            = "http://localhost:4566"
+    sns            = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
+    ssm            = "http://localhost:4566"
+    stepfunctions  = "http://localhost:4566"
+    sts            = "http://localhost:4566"
+  }
 }
 
 // SECOUND RESOURCE
 provider "aws" {
     alias = "secound"
-    version = "~> 2.0"
     region = "us-east-2"
+    access_key                  = "foo"
+    secret_key                  = "bar"
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    endpoints {
+      apigateway     = "http://localhost:4566"
+      cloudformation = "http://localhost:4566"
+      cloudwatch     = "http://localhost:4566"
+      dynamodb       = "http://localhost:4566"
+      es             = "http://localhost:4566"
+      ec2            = "http://localhost:4566"
+      firehose       = "http://localhost:4566"
+      iam            = "http://localhost:4566"
+      kinesis        = "http://localhost:4566"
+      lambda         = "http://localhost:4566"
+      route53        = "http://localhost:4566"
+      redshift       = "http://localhost:4566"
+      s3             = "http://localhost:4566"
+      secretsmanager = "http://localhost:4566"
+      ses            = "http://localhost:4566"
+      sns            = "http://localhost:4566"
+      sqs            = "http://localhost:4566"
+      ssm            = "http://localhost:4566"
+      stepfunctions  = "http://localhost:4566"
+      sts            = "http://localhost:4566"
+  }
 }
 
 // INSTACES PRINCIAL
